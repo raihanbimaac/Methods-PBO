955e31904cc0ae7df3fa74dea8b4b9567839189c
raihanbimaac <71834674+raihanbimaac@users.noreply.github.com>
2021-10-07 16:18:05 +0700

    Add files via upload

---
+++ ProjectPengubinan/src/projectpengubinan/PersegiPanjang.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpengubinan;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class PersegiPanjang {
+    // atribut
+    //(kegiatan 1)int panjang;
+    //int lebar;
+    
+    // method
+    //int hitungLuas(){
+        //int luas = this.panjang * this.lebar;
+        //return luas;
+    
+    //}
+    // method
+    //(kegiatan 2)int hitungLuas(int panjang, int lebar){
+        //int luas = panjang * lebar;
+        //return luas;
+    // method untuk mengalikan dua bilangan a dan b
+    static int perkalian(int a, int b){
+        int hasil = a * b;
+        return hasil;
+    }
+    
+    // method
+    int hitungLuas(int panjang, int lebar){
+        int luas = perkalian(panjang, lebar);
+        return luas;
+
+    }
+
+}
